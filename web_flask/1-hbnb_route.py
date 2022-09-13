a069e1e3b2e068c8bb3b578290893339a7965b04
edwardzuniga <4593@holbertonschool.com>
2022-09-13 14:55:43 -0500

    task 1. HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """ That displays "Hello HBNB!" """
+    """ That displays Hello HBNB! """
     return("Hello HBNB!")
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
